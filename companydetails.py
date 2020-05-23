27e6225fba85817efddbc843f8b0c809440d16ef
FrkAk <f.akbulutlar@gmail.com>
2020-05-23 23:22:25 +0300

    Co-authored-by: Ege <e215215@metu.edu.tr> Co-authored-by: znorgaz123@outlook.com <znorgaz123@outlook.com>

--- companydetails.py
+++ companydetails.py
@@ -1,2 +1,3 @@
 #! C:\Program Files (x86)\Python38-32\python
 import cgi
+print("Hello world")
---
+++ index.py
@@ -0,0 +1,9 @@
+#! C:\Users\Psikolojik\AppData\Local\Programs\Python\Python38-32\python.exe
+import cgi
+import webbrowser, os
+webbrowser.open('file://' + os.path.realpath("index.html"))
+
+
+
+
+
