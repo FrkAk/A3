59f5a419075dad3bb01d00f09adbedcd12cea5ae
FrkAk <f.akbulutlar@gmail.com>
2020-05-25 11:09:20 +0300

    search integration

--- index.py
+++ index.py
@@ -7,61 +7,56 @@ htmltable ="""
 <html>
     <head>
         <title>Northern Cyprus Internship Positions</title>
-        <style>
-        html{
-            text-align: center;  
-            font-size: 25px; 
-        }
-        button{
-            font-size : 20px;
-            background-color:cornflowerblue;
-        }
-        table, th, td {
-            border: 1px solid black;
-            background-color:darkgray;
-        }
-        </style>
+         <link rel="stylesheet" href="common.css">
+        
     </head>
     <body>
         <h1>Welcome to Northern Cyprus Internship Website</h1>
-        <p>Please Select what you want to do</p>       
-        <form method = "post">
-            <input type="text" placeholder="Search" name="search"/>
-            <button type = "submit" id = "search" > Search </button> <br> <br>
-            <button type = "button" onclick = "window.location.href='signInUpPage.py'" > Sign In/Sign Up </button>
-        </form>
+        <div class="form-group pull-right">
+            <input type="text" class="search form-control" placeholder="What you looking for?">
+        </div>    
+        <button type = "button" onclick = "window.location.href='signInUpPage.py'" > Sign In/Sign Up </button>
+       
         
-                
-<table style="width:50%; margin-left:auto;margin-right:auto;">
-            <tr>
-              <th>Software Company</th>
-              <th>Position Name</th>
-              <th>Description</th>
-              <th>Expectations</th>
-              <th>Deadline</th>
-            </tr>
+        <div class="table-wrapper">
+            <span class="counter pull-right"></span>
+            <table class="fl-table table table-hover table-bordered results">
+                <thead>
+                <tr>
+                    <th class="col-md-5 col-xs-5">Software Company</th>
+                    <th class="col-md-4 col-xs-4">Position Name</th>
+                    <th class="col-md-3 col-xs-3">Description</th>
+                    <th class="col-md-2 col-xs-2">Expectations</th>
+                    <th class="col-md-1 col-xs-1">Deadline</th>
+                </tr>
+                <tr class="warning no-result">
+                    <td colspan="4"><i class="fa fa-warning"></i> No result</td>
+                 </tr>
+                </thead>
+                <tbody>
            
 """
 print(htmltable)
 i = 0
 positions = db.getinternshippositions()
 htmlrow = """
-         <tr> 
-                <td > <a onclick = "window.location.href='companydetails.py?{i[0]}'"> {i[0]} </a></td>
-                <td>{i[1]}</td>
-                <td>{i[2]}</td>
-                <td>{i[3]}</td>
-                <td>{i[4]}</td>
-            </tr>
+                <tr> 
+                    <th scope="row"> <a onclick = "window.location.href='companydetails.py?{i[0]}'"> {i[0]} </a></th>
+                    <td>{i[2]}</td>
+                    <td>{i[3]}</td>
+                    <td>{i[4]}</td>
+                    <td>{i[5]}</td>
+                </tr>
 """
 
 htmlend = """
+        <tbody>
         </table>
+        <script src="search.js"></script>
         </body>
 </html>
 """
 
-size = db.countRow("INTERNSHIPPOSITION")
 for i in positions:
     print(htmlrow.format(**locals()))
     
